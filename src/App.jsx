ad1e5e1767af2bd9247fcf1cae6ad4aa5ed3099f
Vika <viktorya[etrosyan36@gmail.com>
2024-02-16 10:45:19 +0400

    my homework

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import { useState } from 'react'
+import './App.css'
 
-export default function App() {
+function App() {
+ const arr=['black','green','yellow']
+  const [message,setMessage]=useState('')
+  const handleMessage=(elm)=>{
+    setMessage(elm)
+  }
   return (
-    <div>
-      <h1>Ani branch </h1>
-      {/* code here... */}
+    <div className={`App ${message}`} >
+      <h1>Background is {message} </h1>
+      <div className='App__item'>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+         Praesentium quam optio sunt inventore? Recusandae porro 
+         maiores numquam, fugiat alias expedita odit in dolorem vel,
+          voluptates eum qui nulla consectetur ratione?</p>
+      {
+        arr.map(elm=>{
+          return <button onClick={()=>handleMessage(elm)} 
+          className={`color__button ${elm}`}
+          key={elm}>{elm}</button>
+        })
+      }
+    </div>
     </div>
   )
 }
+export default App

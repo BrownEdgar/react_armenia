36d9729abf51dd998669969536030ac7832b2147
Vika <viktorya[etrosyan36@gmail.com>
2024-02-21 19:23:01 +0400

    homework post

---
+++ src/Posts/App.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect ,useState} from 'react'
+import './App.css'
+export default function App() {
+    const [posts, setposts] = useState({});
+    const [curentIndex,setCurentIndex]=useState(1)
+    const [nextPost, setNextPost] = useState({});
+    
+    useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/posts/${curentIndex}`)
+        .then(res=>res.json())
+        .then(data=>setposts(data))
+    },[curentIndex])
+        
+     useEffect(()=>{
+        fetch(`https://jsonplaceholder.typicode.com/posts/${curentIndex + 1}`)
+        .then(res=>res.json())
+        .then(data=>setNextPost(data))
+    },[curentIndex])
+        
+      const next = () => setCurentIndex(curentIndex +2);
+
+    const prev = () => {
+        if (curentIndex > 1) {
+            setCurentIndex(curentIndex - 2);
+        }
+    };
+//    const next=()=>setCurentIndex(curentIndex+1)
+//    const prev=()=>setCurentIndex(curentIndex -1)
+  return (
+    <div className='App'>
+        <button onClick={prev} disabled={curentIndex === 1} >&#x2190;</button>
+      <div className='App__content'>
+        <span>{posts.id}</span>
+        <h2>{posts.title}</h2>
+        <p>{posts.body}</p>
+      </div>
+        <div className='App__content'>
+        <span>{nextPost.id}</span>
+        <h2>{nextPost.title}</h2>
+        <p>{nextPost.body}</p>
+      </div>
+      <button onClick={next}>&#x2192;</button>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Modal/App'
+import App from './Posts/App'
 
 import './index.css'
 

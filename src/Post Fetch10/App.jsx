2e571b0d9f6e4895fc951f64cd21fda079ff2121
Hayk <hayk.voskanyan96@gmail.com>
2024-02-22 20:36:48 +0400

    Fetch request already ready

--- src/Post Fetch10/App.jsx
+++ src/Post Fetch10/App.jsx
@@ -1,23 +1,47 @@
 import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
+import './Post.scss'
 
 export default function App() {
-  const [post, setPost] = useState({});
-  console.log(post);
-  const [findIndex, setfindIndex] = useState(1);
+  const [post, setPost] = useState({});                  //First Post
+  const [nextPost, setNextPost] = useState({});          //Second Post
+  const [findIndex, setfindIndex] = useState(1);         //Distance between
 
   useEffect(() => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${findIndex}`)
       .then((resp) => resp.json().then((post) => setPost(post)));
   }, [findIndex]);
 
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${findIndex + 1}`)
+      .then((resp) => resp.json().then((nextPost) => setNextPost(nextPost)));
+  }, [findIndex]);
+
+
+
+
+
+
+
   return (
-  <div className="App">
-    <div className="Post">
-      <h4 className="Post__tile">{post.title}</h4>
-      <p className="Post__item">{post.body}</p>
+    <div className="App">
+      <div className="Post">
+        <div className="Post__section" >
+          <p className="Post__id">User Id: <span>{post.id}</span></p>
+          <h4 className="Post__title">{post.title}</h4>
+          <p className="Post__item">{post.body}</p>
+        </div>
+        <div className="Post__section-second">
+          <p className="Post__id">User Id: <span>{nextPost.id}</span></p>
+          <h4 className="Post__title">{nextPost.title}</h4>
+          <p className="Post__item">{nextPost.body}</p>
+        </div>
+        <div>
+          <button onClick={() => setfindIndex(findIndex - 1)} disabled={findIndex === 1} className="Previous__post">Previous Post</button>
+          <button onClick={() => setfindIndex(findIndex + 1)} disabled={findIndex === 100} className="Next__post">Next Post</button>
+        </div>
+      </div>
     </div>
-  </div>
   )
 }

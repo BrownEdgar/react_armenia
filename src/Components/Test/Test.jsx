fa734d5ed3f8708a0650ca1582abf2238f1d54b3
gevv177 <gevhakobyan177@gmail.com>
2024-02-22 22:57:53 +0400

    2 posts drawing

--- src/Components/Test/Test.jsx
+++ src/Components/Test/Test.jsx
@@ -6,20 +6,25 @@ export default function Test() {
         setComment] = useState({})
         const [currentIndex, setCurrentIndex] = useState(1)
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/comments/${currentIndex}`)
+        fetch(`https://jsonplaceholder.typicode.com/comments?_limit=2&_start=${currentIndex}`)
             .then(res => res.json())
             .then(data => setComment(data))
 
     }, [currentIndex])
-
-    const hendleClickNext = ()=> setCurrentIndex(currentIndex + 1)
-    const hendleClickPrev = ()=>setCurrentIndex(currentIndex - 1)
+    const hendleClickNext = ()=> setCurrentIndex(currentIndex + 2)
+    const hendleClickPrev = ()=>setCurrentIndex(currentIndex - 2)
     return (
         <div className='Test'>
             <button onClick={hendleClickPrev} disabled={currentIndex==1}>prev</button>
             <div className='Test__comments'>
-                <p>{comment.email}</p>
-                <h2>{comment.body}</h2>
+                <p>{comment[0].email}</p>
+                <h2>{comment[0].body}</h2>
+                <p>{comment[0].id}</p>
+            </div>
+            <div className='Test__comments'>
+                <p>{comment[1].email}</p>
+                <h2>{comment[1].body}</h2>
+                <p>{comment[1].id}</p>
             </div>
             <button onClick={hendleClickNext} disabled={currentIndex==45}>next</button>
 

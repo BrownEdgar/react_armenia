caaa2fe5b10fc18c8ccd08a5c1180db287065e7d
Hayk <hayk.voskanyan96@gmail.com>
2024-02-24 10:55:01 +0400

    Create 1 fetch request from 2

--- src/Post Fetch10/App.jsx
+++ src/Post Fetch10/App.jsx
@@ -4,39 +4,34 @@ import { useEffect } from "react";
 import './Post.scss'
 
 export default function App() {
-  const [post, setPost] = useState({});                  //First Post
-  const [nextPost, setNextPost] = useState({});          //Second Post
+  const [posts, setPosts] = useState([]);                  //Post
   const [findIndex, setfindIndex] = useState(1);         //Distance between
 
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${findIndex}`)
-      .then((resp) => resp.json().then((post) => setPost(post)));
+    const firstRequest = fetch(`https://jsonplaceholder.typicode.com/posts/${findIndex}`)
+      .then((resp) => resp.json());
+    const secondRequest = fetch(`https://jsonplaceholder.typicode.com/posts/${findIndex + 1}`)
+      .then((resp) => resp.json());
+      Promise.all([firstRequest, secondRequest]).then(data =>{
+        setPosts(data)
+      })
   }, [findIndex]);
-
-  useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${findIndex + 1}`)
-      .then((resp) => resp.json().then((nextPost) => setNextPost(nextPost)));
-  }, [findIndex]);
-
-
-
-
-
-
 
   return (
     <div className="App">
       <div className="Post">
-        <div className="Post__section" >
-          <p className="Post__id">User Id: <span>{post.id}</span></p>
-          <h4 className="Post__title">{post.title}</h4>
-          <p className="Post__item">{post.body}</p>
-        </div>
-        <div className="Post__section-second">
-          <p className="Post__id">User Id: <span>{nextPost.id}</span></p>
-          <h4 className="Post__title">{nextPost.title}</h4>
-          <p className="Post__item">{nextPost.body}</p>
-        </div>
+        {
+          posts.map(elm =>{
+            return(
+              <div className="Post__section" key={elm.id} >
+                <p className="Post__id">User Id: <span>{elm.id}</span></p>
+                <h4 className="Post__title">{elm.title}</h4>
+                <p className="Post__item">{elm.body}</p>
+              </div>
+            )
+          })
+        }
+       
         <div>
           <button onClick={() => setfindIndex(findIndex - 1)} disabled={findIndex === 1} className="Previous__post">Previous Post</button>
           <button onClick={() => setfindIndex(findIndex + 1)} disabled={findIndex === 100} className="Next__post">Next Post</button>

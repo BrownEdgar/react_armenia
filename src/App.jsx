51234a92884f0bcd06c8bdd4cbc20c4596e28348
AniHovsepyann <anihovsepyan004@email.ru>
2024-02-25 22:38:39 +0400

    file: Homework 10 ig

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
 import './App.css'
-
+import Comments from './Comments/Comments'
+import Posts from './Posts/Posts'
+import Todos from './Todos/Todos'
+import Users from './Users/Users'
 
 export default function App() {
-const [colorName, setColorName]=useState('blue')
-
-function changeName(newName) {
-  setColorName(newName)
-}
-
-
-
   return (
-    <div className='App__container'>
-      <h1>background is <span>{colorName}</span></h1>
-      <div className={`App__item $ ${colorName}`}>
-        <h2><span>React events</span>(change background)</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque voluptas quia exercitationem ex ea aut dolorem, ullam iusto harum adipisci natus quos, sunt voluptates fuga!</p>
-        <button onClick={()=>changeName('black')} className='App__black'>black</button>
-        <button onClick={()=>changeName('blue')} className='App__blue'>blue</button>
-        <button onClick={()=>changeName('yellow')} className='App__yellow'>yellow</button>
-      </div>
-
-
-
-
-
-
-
+    <div className='App'>
+      <h1 className='App__heading'>Click bellow to see the homework result!</h1>
+      <Comments />
+      <Posts />
+      <Todos />
+      <Users />
     </div>
   )
 }
+
---
+++ src/Users/Users.jsx
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react'
+import axios from 'axios';
+import './Users.scss'
+
+export default function Users() {
+  const [content, setContent] = useState([]);
+  useEffect(() => {
+    axios({
+      baseURL: import.meta.env.VITE_DB_URL,
+      url: 'users'
+    })
+      .then(res => setContent(res.data))
+  }, [])
+  return (
+    <details>
+      <summary>Users</summary>
+      <div className='Users'>{
+      content.map((elm) => {
+        return <div
+          className='Users__item'
+          key={elm.id}>
+          <div className='Users__id'>{elm.id}</div>
+          {elm.name}
+          
+        </div>
+      })
+    }
+    </div>
+    </details>
+  )
+}

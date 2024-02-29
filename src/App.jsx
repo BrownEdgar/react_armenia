a8b30f1dc88245e9be30826ea67768c76d7c3a74
AniHovsepyann <anihovsepyan004@email.ru>
2024-03-01 00:25:05 +0400

    Form lesson part 1!

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,69 @@
-import React, { useReducer } from 'react'
-import './App.css'
-import reducer, { initialState } from './reducer';
-import { ADD_RANDOM_THREEDIGITS, CHANGE_NAME_PROPETY, COUNT_IN_TOTAL, SHUFFLE_ARRAY, SORT } from './actionTypes';
-
-
+import React, { useState } from 'react'
+import './App.scss'
+import { nanoid } from "nanoid"
 export default function App() {
-	const [state, dispatch] = useReducer(reducer, initialState);
-
-	const handleClick = (actionType) => {
-		dispatch({ type: actionType })
+	const [currentUserId, setCurrentUserId] = useState(null);
+	const [emailArray, setEmailArray] = useState([]);
+	const [users, setUsers] = useState([]);
+	function handleSubmit(e) {
+		e.preventDefault();
+		const form = e.target
+		const { email, username, password } = form
+		const user = {
+			id: nanoid(),
+			email: email.value,
+			username: username.value,
+			password: password.value
+		}
+		if (!emailArray.includes(email.value)) {
+			setEmailArray([...emailArray, email.value])
+			setUsers([...users, user])
+		}else{
+			alert('You should use unique email !!!')
+		}
 	}
-
-
+	const showPassword = (id) => {
+		setCurrentUserId(id)
+	}
 	return (
 		<div className='App'>
-			<h1>State</h1>
-			<div className="datas">
-				<h2>actions : {state.actions}</h2>
-				<h2>name : {state.user.name}</h2>
-				<h2>arr : {JSON.stringify(state.arr)}</h2>
-				<h2>total : {state.total}</h2></div>
-			<button onClick={() => handleClick(SHUFFLE_ARRAY)}>shuffle</button>
-			<button onClick={() => handleClick(ADD_RANDOM_THREEDIGITS)}>add random three digits</button>
-			<button onClick={() => handleClick(CHANGE_NAME_PROPETY)}>change name</button>
-			<button onClick={() => handleClick(SORT)}>sort</button>
-			<button onClick={() => handleClick(COUNT_IN_TOTAL)}>quantity</button>
+			<div className="form">
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="email">Email</label>
+				<input required type="email" name="email" id="email" />
+				<label htmlFor="username">Username</label>
+				<input required type="text" name="" id="username" />
+				<label htmlFor="password">Password</label>
+				<input required type="password" name="" id="password" />
+
+				<input type="submit" value="save" />
+			</form>
+			</div>
+			<hr />
+			<ul>
+				{
+					users.map((elm)=>{
+						return <li className='users' key={elm.id}>
+							<p><span className='email'>email: </span>{elm.email}</p>
+							<p><span className='username'>username: </span>{elm.username}</p>
+							<p className='p-password'>
+								<span>
+									<span className='password'>pass: </span>
+								<span>
+									{currentUserId === elm.id ? elm.password : "*".repeat(8)}
+								</span>
+								</span>
+								{
+								(currentUserId === elm.id) 
+								? <i className="bi bi-eye" onClick={() => { showPassword(null) }}></i>
+								: <i className="bi bi-eye-slash" onClick={() => { showPassword(elm.id) }}></i>
+								}
+							</p>
+						</li>
+					})
+				}
+			</ul>
 		</div>
+		
 	)
 }

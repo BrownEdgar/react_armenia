4c84b522ba2a73bcf4f9c8b6ad4c4ba581adb6c1
Hayk <hayk.voskanyan96@gmail.com>
2024-03-03 12:12:01 +0400

    Created Form with input, 2 password check area

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client'
 // import App from './ModalWindow9/App'
 // import App from './Post Fetch10/App'
 // import App from './Axios 11/AppAxios'
-import App from './Form 13/App'
-
-import './index.css'
+// import App from './Form 13/App'
+import App from './Form Input 14/App'
+// import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

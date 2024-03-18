a46b562fed42bbf50d69a6374d78f4d3e3b4bfba
gevv177 <gevhakobyan177@gmail.com>
2024-03-18 10:59:23 +0400

    Aphorisms1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
-// import Aphorisms from './Components/18.03.24/Aphorisms/Aphorisms'
+import Aphorisms from './Components/18.03.24/Aphorisms/Aphorisms'
 import Local from './Components/18.03.24/local/Local'
 
 
 export default function App() {
     return (
       <div>
-        {/* <Aphorisms /> */}
-        <Local />
+        <Aphorisms />
+        {/* <Local /> */}
         
       </div>
     )

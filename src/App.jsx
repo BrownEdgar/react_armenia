dae96b65b4f154c3a05b1df7469b2f15630a26a1
gevv177 <gevhakobyan177@gmail.com>
2024-03-29 01:48:43 +0400

    todo with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { addRandomNumber } from './features/countSlice'
-import { addUser } from './features/usersSlice'
-
+import { addUser, deleteUser, getAsyncUsers } from './features/usersSlice'
+import {Formik,Form,Field} from 'formik'
+import { nanoid } from '@reduxjs/toolkit'
+import './App.scss'
 
 export default function App() {
  const dispatch = useDispatch()
- const count = useSelector((state)=>state.count)
- const users = useSelector((state)=>state.users)
- const hendleConut = ()=>{
-  dispatch(addRandomNumber())
- }
- const hendleUser=()=>{
-  dispatch(addUser("valod"))
- }
- console.log(users);
+ const users = useSelector((state)=>state.users.data)
+
+  useEffect(() => {
+    dispatch(getAsyncUsers())
+  }, [])
+  const hendleSubmit= ({message},formik) =>{
+    const user = {
+      id:nanoid(8),
+      message,
+    }
+    dispatch(addUser(user))
+    formik.resetForm()
+  }
+  const hendleDel = (userID)=>{
+    dispatch(deleteUser(userID))
+  }
   return (
-    <div>
-      count:{count}
-      <button onClick={hendleConut}>add random  number</button>
-      users:{JSON.stringify(users)}
-      <button onClick={hendleUser}>add valod</button>
+    <div className='Users'>
+      <Formik
+      initialValues={{message:''}}
+      onSubmit={hendleSubmit}>
+        <Form>
+          <div>
+            <Field className='Users__input' type="text" placeholder={"New user name"} name="message"/>
+            <input className='Users__add' type="submit" value={"ADD USER"}/>
+          </div>
+        </Form>
+      </Formik>
+      {
+        users.map((elm)=>{
+          return (
+            <div className='Users__name' key={elm.id}>
+              <p>{elm.name ||elm.message}</p>
+              <button className='Users__del' onClick={()=>hendleDel(elm.id)}>Delete</button>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
--- src/features/usersSlice.js
+++ src/features/usersSlice.js
@@ -1,14 +1,47 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialValues = {
+  data: [],
+  status: "idle",
+  error: null,
+};
+
+export const getAsyncUsers = createAsyncThunk(
+  "users/getAsyncUsers",
+  async () => {
+    const res = await axios("https://jsonplaceholder.typicode.com/users");
+    return res.data;
+  }
+);
 
 const usersSlice = createSlice({
-    name:"users",
-    initialState:[],
-    reducers:{
-        addUser(state,action){
-             state.push(action.payload)
-             return state
-        }
-    }
-})
-export default usersSlice.reducer
-export const {addUser} = usersSlice.actions 
+  name: "users",
+  initialState: initialValues,
+  reducers: {
+    addUser(state, { payload }) {
+      state.data.push(payload);
+      return state;
+    },
+    deleteUser(state, action) {
+      state.data = state.data.filter((user) => user.id !== action.payload);
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAsyncUsers.pending, (state, action) => {
+        state.status = "pending";
+      })
+      .addCase(getAsyncUsers.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = "fulfilled";
+      })
+      .addCase(getAsyncUsers.rejected, (state, action) => {
+        state.status = "failure";
+        state.error = action.error;
+      });
+  },
+});
+
+export default usersSlice.reducer;
+export const { addUser, deleteUser } = usersSlice.actions;

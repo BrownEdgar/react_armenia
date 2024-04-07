7508d433614abe01a5572d6e320154de341ce79b
AniHovsepyann <anihovsepyan004@email.ru>
2024-04-07 20:57:58 +0400

    4th & 5th homeworks together

--- src/redux5slickSlider/App.jsx
+++ src/redux5slickSlider/App.jsx
@@ -1,35 +1,86 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Slider from "react-slick";
+import './App.scss'
 import { useSelector, useDispatch } from 'react-redux'
 import { getAllPosts, getAsyncPosts } from "./features/postSlice";
+import axios from 'axios'
+import MyLoader from "./Loader";
 
 export default function MultipleItems() {
+  const [editable, setEditable] = useState(null);
   const dispatch = useDispatch()
-  const {data, loading} = useSelector(getAllPosts)
-  console.clear()
-console.log(data);
+  const { data, loading } = useSelector(getAllPosts)
   useEffect(() => {
     dispatch(getAsyncPosts())
   }, []);
+
+
+  const handleDelete = (id) => {
+    axios.delete(`http://localhost:3000/posts/${id}`)
+      .then(() => dispatch(getAsyncPosts()))
+      .catch(err => console.log(err))
+  }
+
+  const handleEdit = (id) => {
+    setEditable(id)
+  }
+
+  const handleFinishEdit = (e) => {
+    e.preventDefault()
+    const { title } = e.target
+    const post = {
+      title: title.value
+    }
+    axios.patch(`http://localhost:3000/posts/${editable}`, post)
+      .then(() => dispatch(getAsyncPosts()))
+      .then(() => setEditable(null))
+      .catch(err => console.log(err))
+  }
+
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 2,
-    slidesToScroll: 2
+    slidesToShow: 1,
+    slidesToScroll: 1
   };
-    return (
+  return (
+
+    !loading
+      ?
       <div className="slider-container">
         <Slider {...settings}>
-        
-            {
-              data.map(elm=>{
-                return <div key={elm.id}>{elm.id}</div>
-              })
-            }
-         
-          
+          {
+            data.map(elm => {
+              const alt = `${elm.id}th image of slide`
+              return <div key={elm.id}>
+                <div className="flex-part">
+                  {
+                    editable === elm.id ? (
+                      <form onSubmit={handleFinishEdit}>
+                        <div>
+                          <input name="title" placeholder="Write new title" required type="text" />
+                          <button>done</button>
+                        </div>
+                      </form>
+                    )
+                      : (
+                        <p> {elm.title} </p>
+                      )
+                  }
+                  <i onClick={() => handleEdit(elm.id)} className="bi bi-pencil"></i>
+                </div>
+                <p>{elm.body}</p>
+                <br />
+                <img width='200px' height='200px' src={elm.img} alt={alt} />
+                <button onClick={() => handleDelete(elm.id)}>&#10006;</button>
+              </div>
+            })
+          }
         </Slider>
       </div>
-    );
+      :
+      <MyLoader />
+  )
 }
--- src/redux5slickSlider/features/postSlice.js
+++ src/redux5slickSlider/features/postSlice.js
@@ -1,11 +1,9 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from 'axios'
-
-
 export const getAsyncPosts = createAsyncThunk(
   'posts/getAsyncPosts',
   async () => {
-    const response = await axios('https://jsonplaceholder.typicode.com/posts?_limit=10')
+    const response = await axios('http://localhost:3000/posts')
     return response.data
   }
 )
@@ -14,28 +12,25 @@ const initialState = {
   data: [],
   loading: false
 }
-
 const postSlice = createSlice({
   name: 'posts',
   initialState: initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(getAsyncPosts.pending, (state, {payload})=>{
+    builder.addCase(getAsyncPosts.pending, (state, { payload }) => {
       state.loading = true
     })
-    builder.addCase(getAsyncPosts.fulfilled, (state, {payload})=>{
+    builder.addCase(getAsyncPosts.fulfilled, (state, { payload }) => {
       state.loading = false
       state.data = payload
+
     })
   },
-  selectors : {
-    getAllPosts : state => state
+  selectors: {
+    getAllPosts: state => state
   }
 })
 
 export default postSlice.reducer
 export const { getAllPosts } = postSlice.selectors
-
-
-
 
